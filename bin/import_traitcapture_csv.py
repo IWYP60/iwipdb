0f6ce83c6df8cf3a5e8233c0e36fbca16f8497e6
Kevin Murray <spam@kdmurray.id.au>
2014-09-17 10:58:05 +1000

    Kill old cruft from import_traitcapture_csv.py

--- bin/import_traitcapture_csv.py
+++ bin/import_traitcapture_csv.py
@@ -1,49 +1,12 @@
 #!/usr/bin/env python
-from traitcapture.orm import Accession, User, Species, ENGINE
-import csv
+from traitcapture.orm import Accession, User, Species, Plant
+from csv import DictReader
 from sqlalchemy.orm import sessionmaker, scoped_session
 from sqlalchemy import create_engine
-from datetime import datetime
 
-engine = create_engine(ENGINE)
-session = scoped_session(sessionmaker(bind=engine))
+def main(opts):
+    engine = create_engine()
+    session = scoped_session(sessionmaker(bind=engine))
 
-collector = User(
-    **{
-     "user_name": "kmurray",
-     "given_name": "Kevin",
-     "family_name": "murray",
-     "email": "k.d.murray.91@gmail.com"
-     }
-    )
-
-session.add(collector)
-session.flush()
-session.commit()
-
-species = Species(
-    **{
-     "genus": "Euc",
-     "species": "maculata",
-     "family": "Myrt",
-     "abbreviation": "Eumac"
-     }
-    )
-
-session.add(species)
-session.flush()
-session.commit()
-
-acc_d = {
-    "accession_name": "test_sp",
-    "species_id": 1,
-    "collector_id": 1, 
-    "date_collected": datetime.now()
-    }
-acc = Accession(**acc_d)
-
-session.add(acc)
-session.flush()
-session.commit()
-
-
+    session.flush()
+    session.commit()
